07213b7d3f738a9e17764235df7e7aabfbaf4e05
Canberk Arıcı <45467111+canberkarc@users.noreply.github.com>
2021-03-12 18:26:26 +0300

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Administrator"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Branch"},{"p":"<Unnamed>","l":"BranchEmployee"},{"p":"<Unnamed>","l":"Company"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"Furniture"},{"p":"<Unnamed>","l":"Person"}]
